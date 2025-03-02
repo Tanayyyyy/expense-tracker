7c7e17e7b1b576c22f8089a0dd2e94814056604c
tanay-hugo <tanay@hugosave.com>
2025-03-02 19:14:33 +0530

    DB dependencies

--- backend/src/main/java/com/org/expensetracker/app/dao/impl/UserDAOImpl.java
+++ backend/src/main/java/com/org/expensetracker/app/dao/impl/UserDAOImpl.java
@@ -2,13 +2,23 @@ package com.org.expensetracker.app.dao.impl;
 
 import com.org.app.dto.user.UserEntity;
 import com.org.expensetracker.app.dao.UserDAO;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void createUser(UserEntity userEntity) {
         //todo: make util for DB queries
+        this.jdbcTemplate.execute("""
+        UPDATE User SET id = '1';
+        """);
     }
 }
